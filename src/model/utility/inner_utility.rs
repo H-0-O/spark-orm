6a06ea6e400a2a4bb9f6e41560d37681f7b1a1aa
Hossein <ho.off.acc@gmail.com>
2023-12-14 22:17:35 +0330

    create function restore and init for put default value to inner

--- src/model/utility/inner_utility.rs
+++ src/model/utility/inner_utility.rs
@@ -1,12 +1,47 @@
 use crate::model::base_model::BaseModel;
+use crate::model::InnerState;
 
 pub trait InnerUtility<T> {
     fn fill(&mut self, inner: T);
+
+    fn init(&mut self)
+    where
+        T: Default;
+
+    fn restore_to_default(&mut self)
+    where
+        T: Default;
+
+    fn set_or_default(&mut self, inner: Option<T>)
+    where
+        T: Default;
 }
 
-impl<'a, T> InnerUtility<T> for BaseModel<'a, T>
-{
+impl<'a, T> InnerUtility<T> for BaseModel<'a, T> {
     fn fill(&mut self, inner: T) {
         *self.inner = inner;
+    }
+    fn init(&mut self)
+    where
+        T: Default,
+    {
+        self.fill(T::default());
+    }
+
+    fn restore_to_default(&mut self)
+    where
+        T: Default,
+    {
+        self.set_inner_state(InnerState::Default);
+        self.fill(T::default());
+    }
+    fn set_or_default(&mut self, inner: Option<T>)
+    where
+        T: Default,
+    {
+        match inner {
+            Some(data) => self.fill(data),
+            None => self.restore_to_default(),
+        };
     }
 }
