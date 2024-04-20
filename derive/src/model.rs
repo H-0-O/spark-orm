906ac5b443d599445c7d23ad0c215528c4af455a
Hossein Salehi <ho.off.acc@gmail.com>
2024-04-21 01:30:16 +0330

    FEATURE| UNSTABLE , model now grab the index attributes

--- derive/src/model.rs
+++ derive/src/model.rs
@@ -1,13 +1,10 @@
 use proc_macro::TokenStream;
-use darling::FromMeta;
-use proc_macro2::Ident;
+use darling::{FromMeta};
 
-use quote::{format_ident, quote, ToTokens};
+use quote::{quote, ToTokens};
 use syn::{Attribute, ItemStruct, Type};
 use syn::GenericParam;
-use crate::_model::__Struct;
-use crate::ModelArgs;
-
+use crate::{ModelArgs};
 use crate::utility::GeneratorResult;
 
 const INNER_CRUD_TRAIT_PATH: &str = "spark_orm::model::crud::inner_crud::InnerCRUD";
@@ -103,27 +100,29 @@ fn generate_defined_filed(__struct: &ItemStruct) -> proc_macro2::TokenStream {
         let ident = field.ident.as_ref().unwrap();
         let filed_type = &field.ty;
         let is_generic = is_generic(filed_type);
-        
+
         // this is for handling missing fields without Option enum
-        
-        let mut attrs = 
-            if !has_attr_exists(&field.attrs, "serde(default)") && 
-                !has_attr_exists(&field.attrs , "model(no_default)")
+
+        let mut attrs =
+            if !attr_exists(&field.attrs, "serde(default)") &&
+                !attr_exists(&field.attrs, "no_default")
             {
-            quote!(
+                quote!(
                 #[serde(default)]
             )
-        }else{
-            quote!()
-        };
-        
+            } else {
+                quote!()
+            };
+
         // collect all developer attributes
         field.attrs.iter().for_each(|attr| {
-            attrs = quote!(
-                #attrs
+            if !is_custom_attribute(attr) {
+                attrs = quote!(
+                    #attrs
 
-                #attr
-            );
+                    #attr
+                );
+            }
         });
         let generic_att = if is_generic {
             let deserialize_string = format!("{} : serde::de::DeserializeOwned", filed_type.to_token_stream().to_string());
@@ -162,23 +161,67 @@ fn generate_model_creator_impl(__struct: &ItemStruct, model_args: &ModelArgs) ->
     let model_name = &__struct.ident;
     let proxy_model = syn::Path::from_string(PROXY_MODEL_STRUCT_PATH).unwrap();
     let coll_name = &model_args.coll_name;
+    let register_attributes_function = generate_register_attribute_function(__struct);
     let (impl_generics, type_generics, where_generics) = __struct.generics.split_for_impl();
     quote! {
            impl #impl_generics #model_name #type_generics #where_generics {
-                fn new_model<'a>(db: &'a mongodb::Database) -> #proxy_model<'a , Self>{
+                pub fn new_model<'a>(db: &'a mongodb::Database) -> #proxy_model<'a , Self>{
+                    Self::register_attributes(db , #coll_name);
                     #proxy_model::new(db , #coll_name)
                 }
+
+                #register_attributes_function
             }
         }
 }
 
 /// attr_to_compare must be without # and [] , like serde(default)
-fn has_attr_exists(attrs: &[Attribute], attr_to_compare: &str) -> bool {
+fn attr_exists(attrs: &[Attribute], attr_to_compare: &str) -> bool {
     let mut has_it = false;
-    attrs.iter().for_each(|attr|{
-        if attr.meta.to_token_stream().to_string() == attr_to_compare{
+    attrs.iter().for_each(|attr| {
+        if attr.meta.to_token_stream().to_string() == attr_to_compare {
             has_it = true;
         }
     });
-   has_it 
+    has_it
+}
+
+fn generate_register_attribute_function(__struct: &ItemStruct) -> proc_macro2::TokenStream {
+    let fields = &__struct.fields;
+    let mut indexes = quote!();
+    println!("HEEEEEEEELL");
+    fields.iter().for_each(|field| {
+        if attr_exists(&field.attrs, "index") {
+            let ident = field.ident.to_token_stream().to_string();
+            indexes = quote!(
+                    #indexes
+                   
+                    #ident, 
+            );
+        }
+    });
+    // println!("the indexes {:?}" , indexes.to_string());
+    quote!(
+        pub async fn register_attributes(db: &mongodb::Database , coll_name: &str){
+            let indexes = vec![#indexes];
+            static registerer: std::sync::Once = std::sync::Once::new();
+            //TODO this must be fix
+            
+            // registerer.call_once( async move ||{
+            //     spark_orm::Spark::register_attributes::<Self>(db , indexes , coll_name).await;
+            // });
+        }
+    )
+}
+
+
+fn is_custom_attribute(attr: &Attribute) -> bool {
+    let custom_attributes = [
+        "no_default",
+        "index"
+    ];
+
+    return custom_attributes.iter().any(|c| {
+        c == &attr.meta.to_token_stream().to_string()
+    });
 }
