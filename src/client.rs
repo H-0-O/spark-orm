e7c55f44d344f5f453dc2ac208464b2298a17c87
Hossein Salehi <ho.off.acc@gmail.com>
2024-04-30 00:33:55 +0330

    TEMP|UNSTABLE|TEMP| try to remove indexes that not exists att all

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
-
+use spark_orm::macros::{error, trace};
 use mongodb::{Client, Database, IndexModel};
 use mongodb::bson::doc;
 use once_cell::sync::OnceCell;
-use serde::{Deserialize, Serialize};
+use serde::{Serialize};
 use serde::de::DeserializeOwned;
-
+use futures::StreamExt;
 
 use crate::connection::{create_client, create_client_options};
 use crate::error::Error;
@@ -29,6 +29,7 @@ impl Spark {
         port: &str,
         db_name: &str,
     ) -> Arc<Database> {
+        Self::init_logger();
         let client_options = create_client_options(user_name, password, host, port)
             .await
             .unwrap();
@@ -53,6 +54,7 @@ impl Spark {
         port: &str,
         db_name: &str,
     ) -> Database {
+        Self::init_logger();
         let client_options = create_client_options(user_name, password, host, port)
             .await
             .unwrap();
@@ -74,41 +76,145 @@ impl Spark {
         }
     }
 
-    pub fn register_attributes<Model>(db: &Database, attributes: Vec<&str>, coll_name: String)
+    pub fn register_attributes<Model>(db: Arc<Database>, attributes: Vec<&str>, coll_name: String)
         where
             Model: Serialize,
             Model: DeserializeOwned,
             Model: Send,
             Model: Sync,
     {
-        let s_attributes: Vec<String> = attributes.iter().map(|sd| {
-            sd.to_string()
-        }).collect();
+        let attrs = attributes.iter().map(|attr| attr.to_string()).collect::<Vec<String>>();
 
-        let dd = db.clone();
-        let qwe = coll_name.clone();
-        tokio::spawn(async move  {
-            let wq = dd.collection::<Model>(&qwe);
-            // let coll = db.collection::<Model>(coll_name);
-            for attr in s_attributes {
-                let index_model = IndexModel::builder().keys(
-                    doc! {
-                       attr: 1
-                   }
-                ).build();
-                println!("before future");
-                let fu = wq.create_index(
-                    index_model,
-                    None,
-                ).await.unwrap();
+        trace!("Spawn task to register indexes");
+
+        let handlers = tokio::spawn(async move {
+            let coll = db.collection::<Model>(&coll_name);
+            let mut previous_indexes = coll.list_indexes(None).await.unwrap();
+            // println!("Hello {:?} ", previous_indexes);
+
+            // Iterate over existing indexes
+            while let Some(Ok(prev_index)) = previous_indexes.next().await {
+                println!("Index keys: {}", prev_index.keys);
+                let key = prev_index.keys.to_string();
+
+                // Check if the index key exists in the list of attributes
+                if !attrs.contains(&key) {
+                    // If not, drop the index
+                    match coll.drop_index(&key, None).await {
+                        Ok(_) => println!("Index {} dropped successfully", key),
+                        Err(e) => {
+                            eprintln!("Failed to drop index {}: {:?}", key, e);
+                            // Log the error
+                            error!("Failed to drop index {}: {:?}", key, e);
+                        }
+                    }
+                }
+            }
+            // for prev_index in previous_indexes.next() {
+            //     if !attrs.contains(&prev_index) {
+            //         trace!("Remove the {prev_index} index from collection {coll_name} ");
+            //
+            //         if let Err(error) = coll.drop_index(prev_index, None).await {
+            //             println!("the error {} "  ,  error);
+            //             error!("Can't remove previous index : {} " , error);
+            //         }
+            //     }
+            // }
+            let attrs = attrs.iter()
+                .map(|attr| {
+                    let key = attr.to_string();
+                    IndexModel::builder().keys(
+                        doc! {
+                        key : 1
+                }
+                    ).build()
+                }).collect::<Vec<IndexModel>>();
+
+            let result = coll.create_indexes(
+                attrs,
+                None,
+            ).await;
+
+            if let Err(error) = result {
+                error!(
+                    "Can't create indexes : {:?}" ,
+                    error
+                );
             }
         });
-
-        // tokio::spawn(async |x| {
-        //     let coll = db.collection::<Model>(coll_name);
-        //     println!("enter in scoped thread");
-        // 
-        //     println!("finish the scoped thread");
-        // });
+        
+        //     let s_attributes = Arc::new(attributes.iter().map(|&sd| sd.to_string()).collect::<Vec<_>>());
+        //
+        //
+        //     println!("Indexes {:?} " , s_attributes );
+        //
+        //     trace!("Spawn thread to create index");
+        //
+        //     tokio::spawn(async move {
+        //         let coll = db.collection::<Model>(&coll_name);
+        //
+        //         trace!("Drop the previous indexes");
+        //         let mut previous_indexes = coll.list_indexes(None).await.unwrap();
+        //
+        //         while let Some(doc_result) = previous_indexes.next().await {
+        //             let doc = doc_result.unwrap(); // Unwrap the Result to get the document
+        //             let missing_attributes: Vec<_> = doc
+        //                 .keys
+        //                 .iter()
+        //                 .filter(|pre_index| {
+        //                     // Exclude "_id" from the attributes
+        //                     *pre_index.0 != "_id" && !s_attributes.contains(&pre_index.0)
+        //                 })
+        //                 .map(|pre_index| pre_index.0)
+        //                 .collect();
+        //
+        //             println!("Attributes not in s_attributes: {:?}", missing_attributes);
+        //         }
+        //
+        //         //TODO just remove those indexes that not exists now
+        //         // if let Err(error) = coll.drop_indexes(None).await {
+        //         //     error!(
+        //         //         "Can't remove the previous index : {}",
+        //         //         error
+        //         //     );
+        //         // }
+        //         s_attributes.iter().for_each(|attr|{
+        //
+        //             let index_model = IndexModel::builder().keys(
+        //                 doc! {
+        //                    attr: 1
+        //                }
+        //             ).build();
+        //
+        //             trace!("Create the index : {} ", attr);
+        //             let re = async {
+        //                 let create_index_result = coll.create_index(
+        //                     index_model,
+        //                     None,
+        //                 ).await;
+        //                 println!("THE ATTR {} ", attr);
+        //                 if let Err(error) = create_index_result {
+        //                     error!(
+        //                     "Can't create index : {}",
+        //                     error
+        //                 );
+        //                 }
+        //             };
+        //             tokio::spawn(re);
+        //         });
+        //     });
+        //
+        //     // tokio::spawn(async |x| {
+        //     //     let coll = db.collection::<Model>(coll_name);
+        //     //     println!("enter in scoped thread");
+        //     //
+        //     //     println!("finish the scoped thread");
+        //     // });
     }
+
+    
+    fn init_logger(){
+        env_logger::init();    
+    }
+
 }
