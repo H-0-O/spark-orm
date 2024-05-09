bc9062984327c0e3a01f0202f13a31170079a38a
Hossein Salehi <ho.off.acc@gmail.com>
2024-05-10 00:44:36 +0330

    FIX| remove the old tests

--- tests/main.rs
+++ tests/main.rs
@@ -1,176 +1,139 @@
-// use std::sync::Arc;
-// use std::thread::sleep;
-// use std::time::Duration;
-// use mongodb::bson::{doc, Document};
-// use mongodb::{Database, IndexModel};
-// use serde::{Deserialize, Serialize};
-// use spark_orm::{Model, ProxyModelCrud, Spark};
-// use spark_orm::model::crud::inner_crud::InnerCRUD;
-// use spark_orm::model::Prototype;
-// 
-// #[Model(coll_name = "users")]
-// #[derive(Serialize, Deserialize, Default, Debug)]
-// struct User {
-//     #[index]
-//     age: u32,
-//     #[index]
-//     name: String,
-//     email: String,
-// }
-// 
-// #[Model(coll_name = "products")]
-// #[derive(Serialize, Deserialize, Default, Debug)]
-// struct Product {
-//     #[index]
-//     age: u32,
-//     #[index]
-//     name: String,
-//     email: String,
-// }
-// 
-// 
-// #[tokio::test]
-// async fn create() {
-//     let db = get_db().await;
-//      
-//     let mut user = User::new_model(&db);
-//     user.name = "Hossein".to_string();
-//     user.email = "spark_orm_test".to_string();
-//     let re = user.save().await;
-// 
-//     // This unwraps the result and return insert ID
-//     re.unwrap();
-// }
-// 
-// 
-// 
-// 
-// 
-// #[tokio::test]
-// async fn find_one() {
-//     let db = get_db().await;
-//     let mut user = User::new_model(&db);
-//     let sample = doc! {
-//         "name" : "Hossein",
-//         "email" : "spark_orm_test"
-//     };
-//     let re = user.find_one(Prototype::Doc(sample)).await;
-// 
-//     let founded = re.unwrap();
-// 
-//     if founded.is_some() {
-//         println!("The result {:?} ", founded.unwrap());
-//     } else {
-//         println!("The result is empty");
+use std::str::FromStr;
+use std::sync::Arc;
+use mongodb::bson::doc;
+use mongodb::bson::oid::ObjectId;
+use mongodb::Database;
+use serde::{Deserialize, Serialize};
+use spark_orm::Spark;
+use spark_orm_derive::Model;
+
+#[Model(coll_name = "users")]
+#[derive(Serialize, Deserialize, Default, Debug)]
+struct User {
+    #[index]
+    name: String,
+    age: u64,
+}
+
+
+//TODO test From trait with struct that has generic
+#[Model(coll_name = "users")]
+#[derive(Serialize, Deserialize, Default, Debug)]
+struct Product {
+    name: String,
+}
+
+// impl From<Product> for Document{
+//     fn from(value: Product) -> Self {
+//         let result = mongodb::bson::to_document(&value);
+//         match result {
+//             Ok(doc) => doc,
+//             Err(_) => doc! {}
+//         }
 //     }
 // }
-// 
-// 
-// async fn get_db() -> Arc<Database> {
-//     Spark::global_connect("root", "123", "localhost", "6789", "rm_orm_db").await
-// }
-// 
-// 
-// #[tokio::test]
-// async fn __find_one() {
-//     let db = get_db().await;
-//     let col = &db.collection::<Document>("users");
-//     let ds = doc! {
-//         "name" : "Hossein"
-//     };
-//     let re = col.find_one(ds, None).await;
-//     let res = re.unwrap();
-//     if res.is_some() {
-//         println!("the ")
-//     } else {
-//         println!("Nothing");
-//     }
-// }
-// 
-// #[tokio::test]
-// async fn find() {
-//     let db = get_db().await;
-//     let we = doc! {
-//         "name" : "Hossein"
-//     };
-//     let mut raw_re = User::find(we, &db, "users").await.unwrap();
-//     while raw_re.advance().await.unwrap() {
-//         println!("the doc is {:?} ", raw_re.deserialize_current().unwrap());
-//     }
-// }
-// 
-// #[tokio::test]
-// async fn find_with_callback() {
-//     let db = get_db().await;
-// 
-//     let we = doc! {
-//         "name" : "Hossein"
-//     };
-//     User::find_with_callback(
-//         we,
-//         |f| {
-//             println!("the User is {:?}", f.name);
-//         },
-//         &db,
-//         "users",
-//     ).await;
-// }
-// 
-// #[tokio::test]
-// async fn soft_delete(){
-//     todo!()
-// }
-// #[tokio::test]
-// async fn force_delete(){
-//     todo!()
-// }
-// 
-// async fn on_create(){
-//     todo!()
-// }
-// 
-// async fn on_created(){
-//     todo!()
-// }
-// 
-// #[tokio::test]
-// async fn index_attribute(){
-//     let db = get_db().await;
-//     let mut user = User::new_model(&db);
-//     user.name = "Hossein".to_string();
-//     
-//     user.save().await.unwrap();
-// 
-// 
-//     sleep(Duration::from_secs(15));
-// 
-//     // let index_model = IndexModel::builder().keys(
-//     //     doc! {
-//     //         "user_name": 1
-//     //     }
-//     // ).build();
-//     // let coll = &db.collection::<Document>("users");
-//     // let result = coll.create_index(
-//     //     index_model,
-//     //     None,
-//     // ).await;
-// 
-//     // println!(
-//     //     "the result {result:?}"
-//     // );
-// 
-//     // struct User{
-//     //     #[index]
-//     //     user_name: String
-//     // }
-// }
-// 
-// async fn unique_attribute(){
-//     // struct User{
-//     //     // TODO in parenthesis must write error message
-//     //     #[unique (" the user name :user_name is exists ") ]
-//     //     user_name: String
-//     // }
-// }
-// 
-// 
+
+
+#[tokio::test]
+async fn save() {
+    let db = get_db().await;
+    let mut user_model = User::new_model(Some(&db));
+    user_model.name = "Hossein".to_string();
+    user_model.save(None).await.unwrap();
+}
+
+#[tokio::test]
+async fn find_one() {
+    let db = get_db().await;
+    let user_model = User::new_model(Some(&db));
+    let mut sample = User::default();
+    sample.name = "Hossein".to_string();
+    let founded = user_model.find_one(
+        sample,
+        None,
+    ).await.unwrap();
+    println!("The founded object {:?} ", founded);
+}
+
+#[tokio::test]
+async fn update() {
+    let db = get_db().await;
+    let mut user_model = User::new_model(Some(&db));
+    user_model._id = Some(ObjectId::from_str("663a7a27cc6093d989a1e279").unwrap());
+    user_model.name = "Hossein 2".to_string();
+    user_model.age = 58;
+    user_model.save(None).await.unwrap();
+}
+
+#[tokio::test]
+async fn update_with_doc() {
+    let db = get_db().await;
+    let user_model = User::new_model(Some(&db));
+    let updated = user_model.update(
+        doc! {
+            "name": "Hossein",
+        },
+        doc! {
+            "$set": {
+                "name": "Hossein 33"
+            }
+        },
+        None,
+    ).await.unwrap();
+    println!("The Updated info {:?}", updated);
+}
+
+#[tokio::test]
+async fn update_with_model() {
+    let db = get_db().await;
+    let user_model = User::new_model(Some(&db));
+    let mut sample_user = User::default();
+    sample_user.name = "Hossein 33".to_string();
+    let updated = user_model.update(
+        &sample_user,
+        doc! {
+            "$set": {
+                "name": "Hossein 3355"
+            }
+        },
+        None,
+    ).await.unwrap();
+
+    println!("The Updated info {:?}", updated);
+}
+
+#[tokio::test]
+async fn update_with_model_instance() {
+    let db = get_db().await;
+    let mut user_model = User::new_model(Some(&db));
+    user_model.name = "Hossein 3355".to_string();
+    user_model.age = 58;
+    let updated = user_model.update(
+        &user_model,
+        doc! {
+            "$set": {
+                "name": "Hossein 325"
+            }
+        },
+        None,
+    ).await.unwrap();
+    println!("The Updated info {:?}", updated);
+}
+
+#[tokio::test]
+async fn find_and_collect() {
+    let db = get_db().await;
+    let user_model = User::new_model(Some(&db));
+
+    let users = user_model.find_and_collect(
+        doc! {"name": "Hossein 2"},
+        None,
+    ).await.unwrap();
+    
+    println!("The users {users:?} ")
+}
+
+
+async fn get_db() -> Arc<Database> {
+    Spark::global_connect("root", "123", "localhost", "6789", "rm_orm_db").await
+}
